3c600865dc73de087bd1d2c8ea52d47d3baffe2f
Myanmar Anonymous Helper Team <84011726+myanmaranonymoushelperteam@users.noreply.github.com>
2021-11-24 21:14:56 +0630

    Add files via upload

---
+++ zuzu.sh
@@ -0,0 +1,108 @@
+
+termux-open https://m.facebook.com/eikyaw.kaung.lay.ma.shi.9987?mds=%2Ftimeline%2Fprofilepic%2Fchooser%2F%3Fpp_source%3Dtimeline&mdf=1
+
+clear
+
+echo -e "\e[1;31m
+
+🍃🌹🍃💕
+💕          💕🍃🌹🍃💕
+💕                💕🍃
+💕    ( N42 )    💕
+💕`    💗    ` 💕
+💕           💕 
+💕            💕 
+💕             💕
+💕( M.A.H.T ) 💕
+💕`   💗  ` 💕
+💕      💕 
+💕   💕 
+💕 💕
+
+
+\e[0m"
+
+echo ""
+
+echo -e "\e[1;32m        Code By Hackcat zuzueisan(N42)  \e[0m"
+
+echo ""
+
+echo -e "\e[1;32m M.A.H.T Lesson Course! \e[0m"
+
+echo ""
+
+echo " 1. Report & Lock ( movie lesson ) 15000 kyat "
+echo""
+echo " 2. Report & Lock ( letter lesson ) 15000 kyat"
+echo ""
+echo " 3. Facebook ADS Boost Lesson ( movie lesson ) 2000 kyat"
+echo ""
+echo " 4. Hacking Basic Knowledge Lesson  5000 kyat"
+echo ""
+echo " 5. Tool Owner create Phishing Lesson 5000 kyat"
+echo ""
+echo " 6. Android Development Phishing Lesson 5000 kyat"
+echo ""
+echo " 7. Android Studio Lesson for Pc 10000 kyat"
+echo ""
+echo " 8. Facebook marketing manager class 2000 kyat"
+
+echo ""
+read -p " 😈Choose what you want to Click : " ghost
+
+if [[ $ghost == 1 ]];then
+echo ""
+sleep 3 
+termux-open  https://www.facebook.com/groups/536338961017855/?ref=share
+
+echo ""
+echo -e "\e[1;32m  Wellcome To M.A.H.T Lesson Course "
+
+elif [[ $ghost == 2 ]];then
+echo ""
+sleep 3
+termux-open  https://www.facebook.com/groups/4371575249554564/?ref=share
+echo ""
+echo -e "\e[1;32m  Wellcome To M.A.H.T Lesson Course "
+
+elif [[ $ghost == 3 ]];then
+echo ""
+sleep 3 
+termux-open  https://www.facebook.com/groups/352947832964336/?ref=share
+echo ""
+echo -e "\e[1;32m  Wellcome To M.A.H.T Lesson Course "
+
+elif [[ $ghost == 4 ]];then
+echo ""
+sleep 3 
+termux-open  https://www.facebook.com/groups/4144238072297742/?ref=share
+echo ""
+echo -e "\e[1;32m  Wellcome To M.A.H.T Lesson Course "
+
+elif [[ $ghost == 5 ]];then
+echo ""
+sleep 3 
+termux-open  https://www.facebook.com/groups/4144238072297742/?ref=share
+echo ""
+echo -e "\e[1;32m  Wellcome To M.A.H.T Lesson Course "
+
+elif [[ $ghost == 6 ]];then
+echo ""
+sleep 3 
+termux-open  https://www.facebook.com/groups/533944837706828/?ref=share
+echo ""
+echo -e "\e[1;32m  Wellcome To M.A.H.T Lesson Course "
+
+elif [[ $ghost == 8 ]];then
+echo ""
+sleep 3 
+termux-open  https://www.facebook.com/groups/1251863491946366/?ref=share_group_link
+echo ""
+echo -e "\e[1;32m  Wellcome To M.A.H.T Lesson Course "
+
+
+
+else
+echo -e "\e[1;33m Villeger Is all Fucking Brain \e[0m"
+fi
